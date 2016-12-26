6698225185de266a60363ccecebd2a3611b338a1
Rafael Cisneros Gómez <rafael.cisneros.gomez@gmail.com>
2016-12-26 04:35:30 -0600

    Aplicando Off-Canvas
    Off Canvas primera vista.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(e) {
-   
+
+   var header = document.querySelector("header");
+   var ul = document.querySelector("nav > ul");
+   console.log(ul);
+   header.addEventListener('click', function(e) {
+      ul.classList.toggle("open");
+      });
+
 });
